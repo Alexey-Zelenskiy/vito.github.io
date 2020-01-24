22bab362f7dcda475947681153590e5535d6710e
Alexey Zelenskiy <zelenskii1999@mail.ru>
2020-01-25 02:35:15 +0700

    commit

---
+++ precache-manifest.69be97a804b711a5a65924fc4aaa29cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6b14bc4fe6d3f2d1111615f2ba4701",
+    "url": "/vito.github.io/index.html"
+  },
+  {
+    "revision": "2acf02ed415f203609bc",
+    "url": "/vito.github.io/static/css/main.d4e3c543.chunk.css"
+  },
+  {
+    "revision": "9bd687ea344e95266135",
+    "url": "/vito.github.io/static/js/2.356f591a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/vito.github.io/static/js/2.356f591a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2acf02ed415f203609bc",
+    "url": "/vito.github.io/static/js/main.1a810e39.chunk.js"
+  },
+  {
+    "revision": "13cfbdfa34ae504c953d",
+    "url": "/vito.github.io/static/js/runtime-main.eb5c9f92.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7d5a0f3de2d07932c951c5732f7de246.js"
+  "/vito.github.io/precache-manifest.69be97a804b711a5a65924fc4aaa29cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vito.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
