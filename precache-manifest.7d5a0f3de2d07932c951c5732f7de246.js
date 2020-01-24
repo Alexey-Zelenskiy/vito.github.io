ff629b70c930fb88e6cd8a7869412ff6141b960b
Alexey Zelenskiy <zelenskii1999@mail.ru>
2020-01-25 02:16:10 +0700

    first commit

---
+++ precache-manifest.7d5a0f3de2d07932c951c5732f7de246.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb07be5378734326ff4dc460d2138cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f0fd418653a38ee6595",
+    "url": "/static/css/main.d4e3c543.chunk.css"
+  },
+  {
+    "revision": "c5f5d8e7daa760dc1239",
+    "url": "/static/js/2.303fa100.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.303fa100.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f0fd418653a38ee6595",
+    "url": "/static/js/main.3e823f2f.chunk.js"
+  },
+  {
+    "revision": "f84539d280f70b7ab6d5",
+    "url": "/static/js/runtime-main.0d36e2b1.js"
+  }
+]);
